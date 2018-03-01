0fae6d97fdcab3a2dacb1683906a4358c513b77c
Alec O’Connor <Alec@AlecOConnor.com>
2018-03-01 10:42:09 -0600

    Initial Commit

---
+++ MLB Roster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MLB Roster
+//
+//  Created by Alec O'Connor on 3/1/18.
+//  Copyright © 2018 Alec O'Connor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
